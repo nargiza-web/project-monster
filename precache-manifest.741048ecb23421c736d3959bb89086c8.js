afad774e73f861f571eaa45872cf880409906663
Nargiza O <nargiza2k@gmail.com>
2019-10-28 18:24:41 -0500

    Updates

---
+++ precache-manifest.741048ecb23421c736d3959bb89086c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0eec7fc1eca69664300c1e73eacf2b",
+    "url": "/project-monster/index.html"
+  },
+  {
+    "revision": "1ac0ecf926e887931d90",
+    "url": "/project-monster/static/css/main.ce18c918.chunk.css"
+  },
+  {
+    "revision": "07bcf9f5f05f07401e44",
+    "url": "/project-monster/static/js/2.07a6689f.chunk.js"
+  },
+  {
+    "revision": "1ac0ecf926e887931d90",
+    "url": "/project-monster/static/js/main.db7b1733.chunk.js"
+  },
+  {
+    "revision": "49cff5c1c762c2969c8a",
+    "url": "/project-monster/static/js/runtime-main.a27a22ea.js"
+  }
+]);
